ab37992d92cfeb3586260e2529bbd2de46ba2b91
Abdelrahman Ebeid <mano@Abdelrahmans-MacBook-Air.local>
2025-10-23 10:59:58 +0400

    seo enhancements

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -15,22 +15,6 @@ import { Helmet } from "react-helmet-async";
 export default function Home() {
   return (
     <>
-      <Helmet>
-        <title>car recovery in dubai</title>
-        <meta
-          name="description"
-          content="Tareeqk offers fast and reliable 24/7 car recovery and roadside assistance in Dubai. Book now or download our app."
-        />
-        <meta
-          name="keywords"
-          content="Car recovery in Dubai, سحب السيارات في دبي,
-            Best car recovery in Dubai, أفضل خدمة سحب سيارات في دبي,
-            Roadside assistance in Dubai, خدمة المساعدة على الطريق في دبي,
-            Desert car recovery service in Dubai, خدمة سحب السيارات في صحراء دبي,
-            Car recovery service in Dubai, خدمة سحب السيارات في دبي,
-            Tareeqk, Vehicle towing Dubai, Emergency car towing UAE"
-          />
-      </Helmet>
       <div>
         <LandingPage />
         <CJI />
